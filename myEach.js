537bb6e23728ad6dbe8cbc7373ac0a96607eaf58
Christopher Pineda <Christopher.Pineda59@myhunter.cuny.edu>
2022-02-25 15:06:17 -0500

    Added myMap implementation with small test code

--- myEach.js
+++ myEach.js
@@ -2,34 +2,25 @@ Array.prototype.myEach = function (perform_array) {
   for (let j = 0; j < this.length; j++) {
     //"this" refers to the array
     if (this[j] === undefined) {
-      continue;
+      continue; //if there is a missing value in the array, go to the next iteration
     }
     perform_array(this[j], j, this); // perform_array is "console.log(x,i,myArray)"
   }
 };
 
-// TEST //
-// Test myEach against the native forEach to ensure that myEach works as the same as forEach
-let myArray = [1, 2, , 4, 5]; // Array called by the function
+Array.prototype.myMap = function (perform_array) {
+  let new_array = [];
+  for (let i = 0; i < this.length; i++) {
+    if (this[i] == undefined) {
+      new_array.push(this[i]); //Push the empty null value into the new array if encountered
+      continue;                //If element is missing then go to next iteration
+    }
+    new_array.push(perform_array(this[i], i, this));
+  }
+  return new_array;
+};
 
-// Test with 1 parameter: element
-console.log("myEach (1 parameter): element");
-myArray.myEach((x) => console.log(x)); // Parameter: x => console.log(x)
-console.log("forEach (1 parameter): element");
-myArray.forEach((x) => console.log(x)); // Parameter: x => console.log(x)
-
-// Test with 2 parameters: element, index
-console.log("myEach (2 parameters): element, index");
-myArray.myEach((x, i) => console.log(x, i)); // Parameter: (x,i) => console.log(x,i)
-console.log("forEach (2 parameters): element, index");
-myArray.forEach((x, i) => console.log(x, i)); // Parameter: (x,i) => console.log(x,i)
-
-// Test with 3 parameters: element, index, array
-console.log("myEach (3 parameters): element, index, array");
-myArray.myEach((x, i, myArray) => console.log(x, i, myArray)); // Parameter: (x,i,myArray) => console.log(x,i,myArray)
-console.log("forEach (3 parameters): element, index, array");
-myArray.forEach((x, i, myArray) => console.log(x, i, myArray)); // Parameter: (x,i,myArray) => console.log(x,i,myArray)
-
-// Original array is not affected
-console.log("Original array:");
-console.log(myArray);
+//Testing myMap 
+myarray = [1,2,3,4];
+result = myarray.myMap(x => x * 2)
+console.log(result)
