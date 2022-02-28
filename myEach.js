903b324cba908665dc804b06f9d5c12315aba0c8
Christopher Pineda <Christopher.Pineda59@myhunter.cuny.edu>
2022-02-28 13:44:12 -0500

    Added Finalizing Comments

--- myEach.js
+++ myEach.js
@@ -17,88 +17,88 @@ Array.prototype.myMap = function (perform_array) {
     }
     new_array.push(perform_array(this[i], i, this));
   }
-  return new_array;
+  return new_array; //returning new array that was pushed in new values
 };
 
 // SOME //
 Array.prototype.mySome = function (perform_array) {
   for (let i = 0; i < this.length; i++) {
-    if (perform_array(this[i], i, this) == true) {
+    if (perform_array(this[i], i, this) == true) { //if element passes the test of the provided function, return true
       return true;
     }
   }
-  return false;
+  return false; //otherwise return false
 };
 
 // REDUCE //
 Array.prototype.myReduce = function (perform_array) {
-  let previous_value = this[0];
+  let previous_value = this[0]; //starting at first index
   for (let i = 1; i < this.length; i++) {
-    index = i;
-    previous_value = perform_array(previous_value, this[i], index);
+    index = i; //recording index
+    previous_value = perform_array(previous_value, this[i], index); 
   }
   return previous_value;
 };
 
 // INCLUDES //
 Array.prototype.myIncludes = function (perform_array, from_index = 0) {
-  if (from_index >= this.length) {
+  if (from_index >= this.length) { //if the starting index is larger than the array size, return false
     return false;
   }
-  for (let i = from_index; i < this.length; i++) {
-    if (this[i] === perform_array) {
+  for (let i = from_index; i < this.length; i++) { 
+    if (this[i] === perform_array) { //if element is strictly equal to the value we are searching for, then return true
       return true;
-    } else if (Number.isNaN(this[i]) && Number.isNaN(perform_array)) {
+    } else if (Number.isNaN(this[i]) && Number.isNaN(perform_array)) { //if both values are NaN, return true
       return true;
     }
   }
-  return false;
+  return false; //otherwise return false
 };
 
 // INDEXOF //
 Array.prototype.myIndexOf = function (perform_array, starting_value = 0) {
-  if (starting_value < 0) {
-    for (let i = this.length + starting_value; i < this.length; i++) {
-      if (this[i] === perform_array) {
+  if (starting_value < 0) { //if the starting value is negative
+    for (let i = this.length + starting_value; i < this.length; i++) { //since value is negative, subtract array length with starting value 
+      if (this[i] === perform_array) { //if element is equal to value we are searching for, return the index number
         return i;
       }
     }
   }
-  else if (starting_value >= 0) {
+  else if (starting_value >= 0) { //if starting value is equal to zero or positive
     for (let i = starting_value; i < this.length; i++) {
       if (this[i] === perform_array) {
         return i;
       }
     }
   }
-  return -1;
+  return -1; //return -1 otherwise
 };
 
 // PUSH //
 Array.prototype.myPush = function (...added_elements) {
   let iterator = 0;
   let added_index = 0;
-  let combined_array_length = this.length + added_elements.length;
-  for (let i = this.length; i < combined_array_length; i++) {
+  let combined_array_length = this.length + added_elements.length; //adding both lengths to limit where i will stop in for loop
+  for (let i = this.length; i < combined_array_length; i++) { //start from the end of the array (length) to start adding new values
     this[i] = added_elements[added_index];
-    added_index++;
+    added_index++; //increment added_index 
   }
   for (let i = 0; i < this.length; i++) {
-    iterator++;
+    iterator++; //increment iterator
   }
   return iterator;
 };
 
 // LASTINDEXOF //
 Array.prototype.myLastIndexOf = function (search_array, starting_value = this.length - 1) {
-  if (starting_value < 0) {
-    for (let i = this.length + starting_value; i >= 0; i--) {
+  if (starting_value < 0) { //check if negative, because if it is negative, the for loop condition changes
+    for (let i = this.length + starting_value; i >= 0; i--) { //Moving backwards 
       if (this[i] === search_array) {
         return i;
       }
     }
   }
-  for (let i = starting_value; i >= 0; i--) {
+  for (let i = starting_value; i >= 0; i--) {//check if positive, because if it is negative, the for loop condition changes
     if (this[i] === search_array) {
       return i;
     }
@@ -109,14 +109,14 @@ Array.prototype.myLastIndexOf = function (search_array, starting_value = this.le
 // KEYS //
 Object.myKeys = function (object_letters) {
   let empty_array = [];
-  if (Array.isArray(object_letters) === true) {
+  if (Array.isArray(object_letters) === true) { //different operation if array
     for (let i = 0; i < object_letters.length; i++) {
       empty_array[i] = i;
     }
     return empty_array;
   }
-  for (element in object_letters) {
-    empty_array.push(element);
+  for (element in object_letters) { //for each element in array like object
+    empty_array.push(element); 
   }
   return empty_array;
 };
@@ -125,20 +125,7 @@ Object.myKeys = function (object_letters) {
 Object.myValues = function (object_letters) {
   let empty_array = [];
   for (element in object_letters) {
-    empty_array.push(object_letters[element])
+    empty_array.push(object_letters[element]) //pushing value instead of the element itself
   }
   return empty_array;
 };
-
-/*
-myEach is checked
-myMap is checked
-mySome is checked
-myReduce is checked
-myIncludes is checked
-myIndexOf is fixed/checked
-myPush is checked
-lastIndexOf is checked
-myKeys is fixed/checked
-myValues is checked
-*/
